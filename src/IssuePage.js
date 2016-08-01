33a08527b2e722a4a655b5139436652886aae90f
Nikita Tikhomirov <nikitati@gmail.com>
2016-08-01 15:12:49 +0300

    add loader indication to IssuePage

--- src/IssuePage.js
+++ src/IssuePage.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import './IssuePage.css';
 import { IndexLink } from 'react-router';
+import Loader from 'react-loader';
 
 export default class IssuePage extends Component {
   constructor(props, context) {
     super(props, context);
 
     this.state = {
+      loaded: false,
       data: {},
       user: {}
     };
@@ -18,7 +20,7 @@ export default class IssuePage extends Component {
       .then( response => response.json() )
       .then( json => {
         // todo add loader
-        this.setState({ data: json, user: json.user });
+        this.setState({ data: json, user: json.user, loaded: true });
       })
       .catch(ex => {
         // todo add error message
@@ -32,40 +34,42 @@ export default class IssuePage extends Component {
 
   render() {
     return (
-      <div className='container'>
-        <div className='issue-page'>
-          <div className='issue-page__left'>
-            <a className='link' href={this.state.user.html_url}>
-              <img className='issue-page__avatar' src={this.state.user.avatar_url} role='presentation' />
-              <div>{this.state.user.login}</div>
-            </a>
-          </div>
-          <div className='issue-page__right'>
-            <div className='issue-page__header'>
-              <a className='link issue-page__title' href={this.state.data.html_url}>
-                {this.state.data.title}
+      <Loader loaded={this.state.loaded}>
+        <div className='container'>
+          <div className='issue-page'>
+            <div className='issue-page__left'>
+              <a className='link' href={this.state.user.html_url}>
+                <img className='issue-page__avatar' src={this.state.user.avatar_url} role='presentation' />
+                <div>{this.state.user.login}</div>
               </a>
-              <div className='issue-page__info'>
-                #{this.state.data.number} openned {moment(this.state.data.created_at).fromNow()}
-              </div>
             </div>
-            <div className='issue-page__body'>
-              <div>
-                status: {this.state.data.state}
+            <div className='issue-page__right'>
+              <div className='issue-page__header'>
+                <a className='link issue-page__title' href={this.state.data.html_url}>
+                  {this.state.data.title}
+                </a>
+                <div className='issue-page__info'>
+                  #{this.state.data.number} openned {moment(this.state.data.created_at).fromNow()}
+                </div>
               </div>
+              <div className='issue-page__body'>
+                <div>
+                  status: {this.state.data.state}
+                </div>
 
-              <div>
-                comments: {this.state.data.comments}
-              </div>
+                <div>
+                  comments: {this.state.data.comments}
+                </div>
 
-              <div>
-                {this.state.data.body}
+                <div>
+                  {this.state.data.body}
+                </div>
               </div>
             </div>
           </div>
+          <IndexLink to="/" className='link'>go to Home</IndexLink>
         </div>
-        <IndexLink to="/" className='link'>go to Home</IndexLink>
-      </div>
+      </Loader>
     );
   }
 }
